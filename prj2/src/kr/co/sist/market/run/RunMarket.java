09be4c81ef3813ff70e9ecc478f6061e5bd694f0
user <user@user-PC>
2017-05-23 16:47:54 +0900

    혜림 - view추가

--- prj2/src/kr/co/sist/market/run/RunMarket.java
+++ prj2/src/kr/co/sist/market/run/RunMarket.java
@@ -3,7 +3,7 @@ package kr.co.sist.market.run;
 public class RunMarket {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub.
 
 	}
 
