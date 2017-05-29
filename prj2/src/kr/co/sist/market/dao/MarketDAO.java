f5b4450c909c019814ab5e188f08d5a81b50e494
user <user@user-PC>
2017-05-29 16:37:59 +0900

    혜림 - view

--- prj2/src/kr/co/sist/market/dao/MarketDAO.java
+++ prj2/src/kr/co/sist/market/dao/MarketDAO.java
@@ -41,7 +41,7 @@ public class MarketDAO {
 		Properties prop=new Properties();
 		try {
 		
-			File file=new File("C:/dev/prj2/sist_prj2/prj2/src/kr/co/sist/market/dao/market.properties");
+			File file=new File("C:/Users/user/git/sist_prj2/prj2/src/kr/co/sist/market/dao/market.properties");
 		
 			if(file.exists()){
 				prop.load(new FileInputStream(file));
--- prj2/src/kr/co/sist/market/evt/SignUpItemViewEvt.java
+++ prj2/src/kr/co/sist/market/evt/SignUpItemViewEvt.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
