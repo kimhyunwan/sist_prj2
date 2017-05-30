659c4095037963078fbaf0aec85f4d3436bcf960
user <user@user-PC>
2017-05-30 15:12:51 +0900

    우동하	0530

--- prj2/src/kr/co/sist/market/evt/SellerInfoViewEvt.java
+++ prj2/src/kr/co/sist/market/evt/SellerInfoViewEvt.java
@@ -3,7 +3,6 @@ package kr.co.sist.market.evt;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
-
 import kr.co.sist.market.view.BuyReqView;
 import kr.co.sist.market.view.MsgWriteView;
 import kr.co.sist.market.view.SellerInfoView;
