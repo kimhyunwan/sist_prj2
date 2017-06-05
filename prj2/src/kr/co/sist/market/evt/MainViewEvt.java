2773f7e0e76bf0cfb09b5aac31901d01f71ea497
user <user@user-PC>
2017-06-05 09:35:04 +0900

    우동하 0605 0934

--- prj2/src/kr/co/sist/market/evt/MainViewEvt.java
+++ prj2/src/kr/co/sist/market/evt/MainViewEvt.java
@@ -4,9 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -26,145 +28,154 @@ import kr.co.sist.market.view.SignUpItemView;
 import kr.co.sist.market.vo.ItemListVO;
 import kr.co.sist.market.vo.MemberInfoVO;
 
-
 public class MainViewEvt extends MouseAdapter implements ActionListener, Runnable {
-   private MainView mv;
-   private MarketDAO m_dao;
-   private CustomerDAO c_dao;
-   private LoginViewEvt lve;
+	private MainView mv;
+	private MarketDAO m_dao;
+	private CustomerDAO c_dao;
+	private LoginViewEvt lve;
 
-   public MainViewEvt(MainView mv){
-      this.mv=mv;
-      m_dao=MarketDAO.getInstance();
-      
-      setItem(0);
-      count();
-   }//MainViewEvtEvt
-   
-   public void count(){
-      c_dao=CustomerDAO.getInstance();
-      String id=lve.id;
-      try {
-         int cntBuyWait=c_dao.selectCntBuyWait(id);
-         int cntSellWait=c_dao.selectCntSellWait(id);
-         int cntMsg=c_dao.selectCntMsg(id);
-         
-         mv.getJlIdRst().setText(id);
-         mv.getJlSellRst().setText(cntSellWait+"건 판매 중");
-         mv.getJlPurchaseRst().setText(cntBuyWait+"건 구매대기 중");
-         mv.getJlNotReadMsgRst().setText(cntMsg+"건 안 읽음");
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-   }
-   
-   public void setItem(int item){
-      DefaultTableModel dtmItem = mv.getDtmItem();
-      try {
-         List<ItemListVO> lstItem = m_dao.selectItemList(item);
-         Object[] rowData=null;
-         ItemListVO iv = null;
-         
-         dtmItem.setRowCount(0); //for문을 돌릴때, 값이 계속 쌓이게 된다는 문제를 해결하기 위해 이거를 써준다.
-         
-         for(int i=0;i<lstItem.size();i++){
-            iv = lstItem.get(i);
-            rowData = new Object[6]; //번호,컬럼명,데이터형,크기 이렇게 총 6개
-            rowData[0]=i+1;
-            rowData[1] = iv.getItemName();
-            rowData[2]=iv.getItemCode();
-            rowData[3]=iv.getItemInfo();
-            rowData[4]=iv.getPrice();
-            rowData[5]=iv.getHiredate();
-            
-            dtmItem.addRow(rowData);
-         }//end for
-      } catch (SQLException e) {
-         JOptionPane.showMessageDialog(mv, "서비스가 원할하지 못합니다.");
-         e.printStackTrace();
-      }//end catch 
-   }//setItem
-   
-   private int typeCode(String itemCode){
-      int code=0;
-      m_dao=MarketDAO.getInstance();
-      
-      try {
-         code=m_dao.selectItemType(itemCode);
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-      
-      return code;
-   }
-   
-   @Override
-   public void mouseClicked(MouseEvent me) {
-      if(me.getClickCount()==2){
-         JTable temp = mv.getJtItemList();
-         int selectedRow = temp.getSelectedRow();
-         String itemCode=(String) temp.getValueAt(selectedRow, 2);
-         ItemListVO iv = new ItemListVO();
-         int typeCode=typeCode(itemCode);
-         iv.setItemCode(itemCode);
-         iv.setItemName((String) temp.getValueAt(selectedRow, 1));
-         iv.setPrice((Integer) temp.getValueAt(selectedRow,4));
-         iv.setItemInfo((String) temp.getValueAt(selectedRow, 3));
-         iv.setHiredate((String) temp.getValueAt(selectedRow, 5));
-         iv.setItemType(mv.getJcbType().getItemAt(typeCode));
-         new ItemInfoView(iv);//띄어주어야할 항목들이 mv가 가지고있으므로 안에 넣어주는 것이다.
-      }//end if
-   }//mouseClicked
-   
-   @Override
-   public void actionPerformed(ActionEvent ae) {
-      if(ae.getSource()==mv.getJbMyInfoCh()){
-         c_dao=CustomerDAO.getInstance();
-         MemberInfoVO miv=new MemberInfoVO();
-         
-         try {
-            miv=c_dao.selectPreMember(lve.id);
-            new MyInfoChView(c_dao, miv);
-         } catch (SQLException e) {
-            e.printStackTrace();
-         }
-      }//end if
-      if(ae.getSource()==mv.getJbSellList()){
-         new SellListView(0);
-      }//end if
-      if(ae.getSource()==mv.getJbBuyList()){
-         new BuyListView(0);
-      }//end if
-      if(ae.getSource()==mv.getJbMsgList()){
-         new MsgListView();
-      }//end if
-      if(ae.getSource()==mv.getJbType()){
-         int item=mv.getJcbType().getSelectedIndex();
-         setItem(item);
-      }//end if
-      
-      if(ae.getSource()==mv.getJbSignUp()){
-         new SignUpItemView(mv);
-      }
-      if(ae.getSource()==mv.getJbMemberLeave()){
-         int flag =JOptionPane.showConfirmDialog(mv, "회원탈퇴를 하시겠습니까?");
-         switch (flag) {
-         case JOptionPane.OK_OPTION:
-            new MemberLeaveView(mv);
-         }//end switch
-      }//end if
-   }
+	public MainViewEvt(MainView mv) {
+		this.mv = mv;
+		m_dao = MarketDAO.getInstance();
 
-   @Override
-   public void run() {
-      while(true){
-      count();
-         try {
-            Thread.sleep(5000);
-         } catch (InterruptedException e) {
-            e.printStackTrace();
-         }
-      }
-   }
+		setItem(0);
+		count();
+	}// MainViewEvtEvt
+
+	public void count() {
+		c_dao = CustomerDAO.getInstance();
+		String id = lve.id;
+		try {
+			int cntBuyWait = c_dao.selectCntBuyWait(id);
+			int cntSellWait = c_dao.selectCntSellWait(id);
+			int cntMsg = c_dao.selectCntMsg(id);
+			String imgName=c_dao.selectImgName(lve.id);
+			File file = new File(System.getProperty("user.dir") + "/src/kr/co/sist/market/img/customer/" + imgName);
+			ImageIcon iiInfo = null;
+			if (!file.exists()) {
+				iiInfo = new ImageIcon(System.getProperty("user.dir") + "/src/kr/co/sist/market/img/default.jpg");
+			} else {
+				iiInfo = new ImageIcon(System.getProperty("user.dir") + "/src/kr/co/sist/market/img/customer/" + imgName);
+			}
+			
+			mv.getJlIdRst().setText(id);
+			mv.getJlSellRst().setText(cntSellWait + "건 판매 중");
+			mv.getJlPurchaseRst().setText(cntBuyWait + "건 구매대기 중");
+			mv.getJlNotReadMsgRst().setText(cntMsg + "건 안 읽음");
+			mv.getLblInfo().setIcon(iiInfo);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setItem(int item) {
+		DefaultTableModel dtmItem = mv.getDtmItem();
+		try {
+			List<ItemListVO> lstItem = m_dao.selectItemList(item);
+			Object[] rowData = null;
+			ItemListVO iv = null;
+
+			dtmItem.setRowCount(0); // for문을 돌릴때, 값이 계속 쌓이게 된다는 문제를 해결하기 위해 이거를
+									// 써준다.
+
+			for (int i = 0; i < lstItem.size(); i++) {
+				iv = lstItem.get(i);
+				rowData = new Object[6]; // 번호,컬럼명,데이터형,크기 이렇게 총 6개
+				rowData[0] = i + 1;
+				rowData[1] = iv.getItemName();
+				rowData[2] = iv.getItemCode();
+				rowData[3] = iv.getItemInfo();
+				rowData[4] = iv.getPrice();
+				rowData[5] = iv.getHiredate();
+
+				dtmItem.addRow(rowData);
+			} // end for
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(mv, "서비스가 원할하지 못합니다.");
+			e.printStackTrace();
+		} // end catch
+	}// setItem
+
+	private int typeCode(String itemCode) {
+		int code = 0;
+		m_dao = MarketDAO.getInstance();
+
+		try {
+			code = m_dao.selectItemType(itemCode);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return code;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent me) {
+		if (me.getClickCount() == 2) {
+			JTable temp = mv.getJtItemList();
+			int selectedRow = temp.getSelectedRow();
+			String itemCode = (String) temp.getValueAt(selectedRow, 2);
+			ItemListVO iv = new ItemListVO();
+			int typeCode = typeCode(itemCode);
+			iv.setItemCode(itemCode);
+			iv.setItemName((String) temp.getValueAt(selectedRow, 1));
+			iv.setPrice((Integer) temp.getValueAt(selectedRow, 4));
+			iv.setItemInfo((String) temp.getValueAt(selectedRow, 3));
+			iv.setHiredate((String) temp.getValueAt(selectedRow, 5));
+			iv.setItemType(mv.getJcbType().getItemAt(typeCode));
+			new ItemInfoView(iv);// 띄어주어야할 항목들이 mv가 가지고있으므로 안에 넣어주는 것이다.
+		} // end if
+	}// mouseClicked
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		if (ae.getSource() == mv.getJbMyInfoCh()) {
+			c_dao = CustomerDAO.getInstance();
+			MemberInfoVO miv = new MemberInfoVO();
+
+			try {
+				miv = c_dao.selectPreMember(lve.id);
+				new MyInfoChView(c_dao, miv);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} // end if
+		if (ae.getSource() == mv.getJbSellList()) {
+			new SellListView(0);
+		} // end if
+		if (ae.getSource() == mv.getJbBuyList()) {
+			new BuyListView(0);
+		} // end if
+		if (ae.getSource() == mv.getJbMsgList()) {
+			new MsgListView();
+		} // end if
+		if (ae.getSource() == mv.getJbType()) {
+			int item = mv.getJcbType().getSelectedIndex();
+			setItem(item);
+		} // end if
+
+		if (ae.getSource() == mv.getJbSignUp()) {
+			new SignUpItemView(mv);
+		}
+		if (ae.getSource() == mv.getJbMemberLeave()) {
+			int flag = JOptionPane.showConfirmDialog(mv, "회원탈퇴를 하시겠습니까?");
+			switch (flag) {
+			case JOptionPane.OK_OPTION:
+				new MemberLeaveView(mv);
+			}// end switch
+		} // end if
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			count();
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
--- prj2/src/kr/co/sist/market/view/MainView.java
+++ prj2/src/kr/co/sist/market/view/MainView.java
@@ -29,262 +29,251 @@ import kr.co.sist.market.evt.MainViewEvt;
 
 /**
  * 탭을 가지고 사용자에게 보여주는 Form
+ * 
  * @author user
  *
  */
 @SuppressWarnings("serial")
 public class MainView extends JFrame {
-   private JTable jtItemList;
-   private DefaultTableModel dtmItem;
+	private JTable jtItemList;
+	private DefaultTableModel dtmItem;
 
+	private JTabbedPane jtpTab;
+	private JComboBox<String> jcbType;
+	private JButton jbType, jbMyInfoCh, jbSellList, jbBuyList, jbSignUp, jbMsgList, jbMemberLeave;
+	private CustomerDAO cd;
+	private LoginViewEvt lve;
+	private JLabel jlIdRst, jlSellRst, jlPurchaseRst, jlNotReadMsgRst, lblInfo;
 
-   private JTabbedPane jtpTab;
-   private JComboBox<String> jcbType;
-   private JButton jbType, jbMyInfoCh, jbSellList, jbBuyList, jbSignUp, jbMsgList, jbMemberLeave;
-   private static CustomerDAO cd;
-   private LoginViewEvt lve;
-   private JLabel jlIdRst, jlSellRst,  jlPurchaseRst, jlNotReadMsgRst;
-   
-   public MainView(CustomerDAO cd) throws SQLException{
-      super("중고장터에 어서오세요!!"); 
-      String id=lve.id;
-      
-      String[] columnNames={"번호","제품명","제품코드","판매 제품 설명","가격","등록일"};
-      String[][] data = {};
-      
-      String[] types = {"----- 전체보기 -----","뷰티/잡화","식품/마트/유아","가구/생활/건강","스포츠/자동차/공구","도서","기타"};
-      jcbType = new JComboBox<String>(types);
-      jbType = new JButton("검색");
+	public MainView(CustomerDAO cd) throws SQLException {
+		super("중고장터에 어서오세요!!");
+		String id = lve.id;
 
+		String[] columnNames = { "번호", "제품명", "제품코드", "판매 제품 설명", "가격", "등록일" };
+		String[][] data = {};
 
-      dtmItem=new DefaultTableModel(data, columnNames){
-         //편집불가
-         @Override
-         public boolean isCellEditable(int row, int column) {
-            return false;
-         }//isCellEditable
-          
-      };
-      
-      jtItemList=new JTable(dtmItem){
-         //컬럼 행에 색 집어넣기
-            public Component prepareRenderer(TableCellRenderer renderer,
-                       int row, int column) {
-                   Component component = super.prepareRenderer(renderer, row,
-                           column);
+		String[] types = { "----- 전체보기 -----", "뷰티/잡화", "식품/마트/유아", "가구/생활/건강", "스포츠/자동차/공구", "도서", "기타" };
+		jcbType = new JComboBox<String>(types);
+		jbType = new JButton("검색");
 
-                       component.setBackground(new Color(0xFAFFFF));
+		dtmItem = new DefaultTableModel(data, columnNames) {
+			// 편집불가
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}// isCellEditable
 
-                   return component;
-               }
-         
-      };
-      
-      // DefaultTableCellHeaderRenderer 생성 (가운데 정렬을 위한)
-      DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
-       
-      // DefaultTableCellHeaderRenderer의 정렬을 가운데 정렬로 지정
-      dtcr.setHorizontalAlignment(SwingConstants.CENTER);
-       
-      // 정렬할 테이블의 ColumnModel을 가져옴
-      TableColumnModel tcm = jtItemList.getColumnModel();
-       
-      // 반복문을 이용하여 테이블을 가운데 정렬로 지정
-      for (int i = 0; i < tcm.getColumnCount(); i++) {
-         tcm.getColumn(i).setCellRenderer(dtcr);
-      }
-      //컬럼을 선택하여 움직이지 못하도록 설정
-      jtItemList.getTableHeader().setReorderingAllowed(false);
-      //컬럼의 높이 설정
-      jtItemList.setRowHeight(50);
-      //컬럼의 넓이 설정
-      jtItemList.getColumnModel().getColumn(0).setPreferredWidth(40);
-      jtItemList.getColumnModel().getColumn(1).setPreferredWidth(150);
-      jtItemList.getColumnModel().getColumn(2).setPreferredWidth(120);
-      jtItemList.getColumnModel().getColumn(3).setPreferredWidth(250);
-      jtItemList.getColumnModel().getColumn(4).setPreferredWidth(70);
-      jtItemList.getColumnModel().getColumn(5).setPreferredWidth(150);
-      
-      JScrollPane jspMenu = new JScrollPane(jtItemList);
-      
-      JLabel jlType = new JLabel("제품분류");
-      JPanel jpTop = new JPanel();
-      jpTop.add(jlType);
-      jpTop.add(jcbType);
-      jpTop.add(jbType);
-      
-      JPanel jpItem = new JPanel();
-      jpItem.setLayout(new BorderLayout());
-      jpItem.add("North", jpTop);
-      jpItem.add("Center", jspMenu);
-      String imgName=cd.selectImgName(lve.id);
-	
-      File file=new File(System.getProperty("user.dir") + "/src/kr/co/sist/market/img/customer/" + imgName);
-		ImageIcon iiInfo=null;
-		if(!file.exists()){
-			iiInfo=new ImageIcon(System.getProperty("user.dir")+"/src/kr/co/sist/market/img/default.jpg");
-		}else{
-			iiInfo = new ImageIcon(System.getProperty("user.dir") + "/src/kr/co/sist/market/img/customer/" + imgName);
+		};
+
+		jtItemList = new JTable(dtmItem) {
+			// 컬럼 행에 색 집어넣기
+			public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+				Component component = super.prepareRenderer(renderer, row, column);
+
+				component.setBackground(new Color(0xFAFFFF));
+
+				return component;
+			}
+
+		};
+
+		// DefaultTableCellHeaderRenderer 생성 (가운데 정렬을 위한)
+		DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
+
+		// DefaultTableCellHeaderRenderer의 정렬을 가운데 정렬로 지정
+		dtcr.setHorizontalAlignment(SwingConstants.CENTER);
+
+		// 정렬할 테이블의 ColumnModel을 가져옴
+		TableColumnModel tcm = jtItemList.getColumnModel();
+
+		// 반복문을 이용하여 테이블을 가운데 정렬로 지정
+		for (int i = 0; i < tcm.getColumnCount(); i++) {
+			tcm.getColumn(i).setCellRenderer(dtcr);
 		}
-      JLabel lblInfo = new JLabel(iiInfo);
-      
-      
-      JLabel jlId = new JLabel("아이디명 : ");
-      JLabel jlSell = new JLabel("판매 대기 현황 : ");
-      JLabel jlPurchase = new JLabel("구매 대기 현황 : ");
-      JLabel jlNotReadMsg = new JLabel("안읽은 메세지 : ");
+		// 컬럼을 선택하여 움직이지 못하도록 설정
+		jtItemList.getTableHeader().setReorderingAllowed(false);
+		// 컬럼의 높이 설정
+		jtItemList.setRowHeight(50);
+		// 컬럼의 넓이 설정
+		jtItemList.getColumnModel().getColumn(0).setPreferredWidth(40);
+		jtItemList.getColumnModel().getColumn(1).setPreferredWidth(150);
+		jtItemList.getColumnModel().getColumn(2).setPreferredWidth(120);
+		jtItemList.getColumnModel().getColumn(3).setPreferredWidth(250);
+		jtItemList.getColumnModel().getColumn(4).setPreferredWidth(70);
+		jtItemList.getColumnModel().getColumn(5).setPreferredWidth(150);
 
-      jlIdRst = new JLabel();
-      jlSellRst = new JLabel();
-      jlPurchaseRst = new JLabel();
-      jlNotReadMsgRst = new JLabel();
-      
-      jbMyInfoCh = new JButton("내 정보 변경");
-      jbSellList = new JButton("판매목록");
-      jbBuyList = new JButton("구매목록");
-      jbSignUp = new JButton("판매물품 등록");
-      jbMsgList = new JButton("메세지 확인");
-      jbMemberLeave = new JButton("회원탈퇴");
-      
-      ImageIcon icon = new ImageIcon(System.getProperty("user.dir")+"/src/kr/co/sist/market/img/background.jpg");
-      JLabel backgroundImg = new JLabel(icon);
-      
-      JPanel jpComInfo = new JPanel();
-      
-      lblInfo.setBounds(30, 30, 180, 195);
-      jlId.setBounds(230, 30, 70, 20);
-      jlSell.setBounds(230, 70, 120, 20);
-      jlPurchase.setBounds(230, 110, 120, 20);
-      jlNotReadMsg.setBounds(230, 150, 110, 20);
-      jlIdRst.setBounds(350, 30, 150, 20);
-      jlSellRst.setBounds(350, 70, 150, 20);
-      jlPurchaseRst.setBounds(350, 110, 150, 20);
-      jlNotReadMsgRst.setBounds(350, 150, 150, 20);
-      jbMyInfoCh.setBounds(530, 30,130 , 20);
-      jbSellList.setBounds(130, 250,200 , 50);
-      jbBuyList.setBounds(400, 250,200 , 50);
-      jbSignUp.setBounds(130, 350,200 , 50);
-      jbMsgList.setBounds(400, 350,200 , 50);
-      jbMemberLeave.setBounds(640, 500,130 , 20);
-      backgroundImg.setBounds(0, 0, 790, 600);
-      
-      jpComInfo.add(lblInfo);
-      jpComInfo.add(jlId);
-      jpComInfo.add(jlSell);
-      jpComInfo.add(jlPurchase);
-      jpComInfo.add(jlNotReadMsg);
-      jpComInfo.add(jlIdRst);
-      jpComInfo.add(jlSellRst);
-      jpComInfo.add(jlPurchaseRst);
-      jpComInfo.add(jlNotReadMsgRst);
-      jpComInfo.add(jbMyInfoCh);
-      jpComInfo.add(jbSellList);
-      jpComInfo.add(jbBuyList);
-      jpComInfo.add(jbSignUp);
-      jpComInfo.add(jbMsgList);
-      jpComInfo.add(jbMemberLeave);
-      jpComInfo.add(backgroundImg);
-      
-      //이벤트 추가
-      MainViewEvt mve = new MainViewEvt(this);
-      jbMyInfoCh.addActionListener(mve);
-      jbSellList.addActionListener(mve);
-      jbBuyList.addActionListener(mve);
-      jbMsgList.addActionListener(mve);
-      jbType.addActionListener(mve);
-      jtItemList.addMouseListener(mve);
-      jbSignUp.addActionListener(mve);
-      jbMemberLeave.addActionListener(mve);
-      //수동배치
-      jpComInfo.setLayout(null);
-      jpComInfo.setBounds(10,10,800,600);
-      
-      jtpTab=new JTabbedPane();
-      jtpTab.add("장터", jpItem);
-      jtpTab.addTab("마이페이지", jpComInfo);
-      
-      add("Center",jtpTab);
+		JScrollPane jspMenu = new JScrollPane(jtItemList);
 
-      
-      addWindowListener(new WindowAdapter(){
+		JLabel jlType = new JLabel("제품분류");
+		JPanel jpTop = new JPanel();
+		jpTop.add(jlType);
+		jpTop.add(jcbType);
+		jpTop.add(jbType);
 
-         @Override
-         public void windowClosing(WindowEvent e) {
-            dispose();
-            System.exit(0);
-         }//windowClosing
-         
-      });
-      
-      setBounds(10,10,800,600);
-      setVisible(true);
-      //창 크기 고정
-      setResizable(false);
-   }//MenuForm
+		JPanel jpItem = new JPanel();
+		jpItem.setLayout(new BorderLayout());
+		jpItem.add("North", jpTop);
+		jpItem.add("Center", jspMenu);
 
-   public JTable getJtItemList() {
-      return jtItemList;
-   }
+		JLabel jlId = new JLabel("아이디명 : ");
+		JLabel jlSell = new JLabel("판매 대기 현황 : ");
+		JLabel jlPurchase = new JLabel("구매 대기 현황 : ");
+		JLabel jlNotReadMsg = new JLabel("안읽은 메세지 : ");
 
-   public DefaultTableModel getDtmItem() {
-      return dtmItem;
-   }
+		lblInfo = new JLabel();
+		jlIdRst = new JLabel();
+		jlSellRst = new JLabel();
+		jlPurchaseRst = new JLabel();
+		jlNotReadMsgRst = new JLabel();
 
-   public JTabbedPane getJtpTab() {
-      return jtpTab;
-   }
+		jbMyInfoCh = new JButton("내 정보 변경");
+		jbSellList = new JButton("판매목록");
+		jbBuyList = new JButton("구매목록");
+		jbSignUp = new JButton("판매물품 등록");
+		jbMsgList = new JButton("메세지 확인");
+		jbMemberLeave = new JButton("회원탈퇴");
 
-   public JComboBox<String> getJcbType() {
-      return jcbType;
-   }
+		ImageIcon icon = new ImageIcon(System.getProperty("user.dir") + "/src/kr/co/sist/market/img/background.jpg");
+		JLabel backgroundImg = new JLabel(icon);
 
-   public JButton getJbType() {
-      return jbType;
-   }
+		JPanel jpComInfo = new JPanel();
 
-   public JButton getJbMyInfoCh() {
-      return jbMyInfoCh;
-   }
+		lblInfo.setBounds(30, 30, 180, 195);
+		jlId.setBounds(230, 30, 70, 20);
+		jlSell.setBounds(230, 70, 120, 20);
+		jlPurchase.setBounds(230, 110, 120, 20);
+		jlNotReadMsg.setBounds(230, 150, 110, 20);
+		jlIdRst.setBounds(350, 30, 150, 20);
+		jlSellRst.setBounds(350, 70, 150, 20);
+		jlPurchaseRst.setBounds(350, 110, 150, 20);
+		jlNotReadMsgRst.setBounds(350, 150, 150, 20);
+		jbMyInfoCh.setBounds(530, 30, 130, 20);
+		jbSellList.setBounds(130, 250, 200, 50);
+		jbBuyList.setBounds(400, 250, 200, 50);
+		jbSignUp.setBounds(130, 350, 200, 50);
+		jbMsgList.setBounds(400, 350, 200, 50);
+		jbMemberLeave.setBounds(640, 500, 130, 20);
+		backgroundImg.setBounds(0, 0, 790, 600);
 
-   public JButton getJbSellList() {
-      return jbSellList;
-   }
+		jpComInfo.add(lblInfo);
+		jpComInfo.add(jlId);
+		jpComInfo.add(jlSell);
+		jpComInfo.add(jlPurchase);
+		jpComInfo.add(jlNotReadMsg);
+		jpComInfo.add(jlIdRst);
+		jpComInfo.add(jlSellRst);
+		jpComInfo.add(jlPurchaseRst);
+		jpComInfo.add(jlNotReadMsgRst);
+		jpComInfo.add(jbMyInfoCh);
+		jpComInfo.add(jbSellList);
+		jpComInfo.add(jbBuyList);
+		jpComInfo.add(jbSignUp);
+		jpComInfo.add(jbMsgList);
+		jpComInfo.add(jbMemberLeave);
+		jpComInfo.add(backgroundImg);
 
-   public JButton getJbBuyList() {
-      return jbBuyList;
-   }
+		// 이벤트 추가
+		MainViewEvt mve = new MainViewEvt(this);
+		jbMyInfoCh.addActionListener(mve);
+		jbSellList.addActionListener(mve);
+		jbBuyList.addActionListener(mve);
+		jbMsgList.addActionListener(mve);
+		jbType.addActionListener(mve);
+		jtItemList.addMouseListener(mve);
+		jbSignUp.addActionListener(mve);
+		jbMemberLeave.addActionListener(mve);
+		// 수동배치
+		jpComInfo.setLayout(null);
+		jpComInfo.setBounds(10, 10, 800, 600);
 
-   public JButton getJbSignUp() {
-      return jbSignUp;
-   }
+		jtpTab = new JTabbedPane();
+		jtpTab.add("장터", jpItem);
+		jtpTab.addTab("마이페이지", jpComInfo);
 
-   public JButton getJbMsgList() {
-      return jbMsgList;
-   }
-   public JButton getJbMemberLeave() {
-      return jbMemberLeave;
-   }
+		add("Center", jtpTab);
 
-   public void setJbMemberLeave(JButton jbMemberLeave) {
-      this.jbMemberLeave = jbMemberLeave;
-   }
+		addWindowListener(new WindowAdapter() {
 
-   public JLabel getJlIdRst() {
-      return jlIdRst;
-   }
+			@Override
+			public void windowClosing(WindowEvent e) {
+				dispose();
+				System.exit(0);
+			}// windowClosing
 
-   public JLabel getJlSellRst() {
-      return jlSellRst;
-   }
+		});
 
-   public JLabel getJlPurchaseRst() {
-      return jlPurchaseRst;
-   }
+		setBounds(10, 10, 800, 600);
+		setVisible(true);
+		// 창 크기 고정
+		setResizable(false);
+	}// MenuForm
 
-   public JLabel getJlNotReadMsgRst() {
-      return jlNotReadMsgRst;
-   }
-   
-   
-   
-}//class
+	public JTable getJtItemList() {
+		return jtItemList;
+	}
+
+	public DefaultTableModel getDtmItem() {
+		return dtmItem;
+	}
+
+	public JTabbedPane getJtpTab() {
+		return jtpTab;
+	}
+
+	public JComboBox<String> getJcbType() {
+		return jcbType;
+	}
+
+	public JButton getJbType() {
+		return jbType;
+	}
+
+	public JButton getJbMyInfoCh() {
+		return jbMyInfoCh;
+	}
+
+	public JButton getJbSellList() {
+		return jbSellList;
+	}
+
+	public JButton getJbBuyList() {
+		return jbBuyList;
+	}
+
+	public JButton getJbSignUp() {
+		return jbSignUp;
+	}
+
+	public JButton getJbMsgList() {
+		return jbMsgList;
+	}
+
+	public JButton getJbMemberLeave() {
+		return jbMemberLeave;
+	}
+
+	public void setJbMemberLeave(JButton jbMemberLeave) {
+		this.jbMemberLeave = jbMemberLeave;
+	}
+
+	public JLabel getJlIdRst() {
+		return jlIdRst;
+	}
+
+	public JLabel getJlSellRst() {
+		return jlSellRst;
+	}
+
+	public JLabel getJlPurchaseRst() {
+		return jlPurchaseRst;
+	}
+
+	public JLabel getJlNotReadMsgRst() {
+		return jlNotReadMsgRst;
+	}
+
+	public JLabel getLblInfo() {
+		return lblInfo;
+	}
+
+}// class
